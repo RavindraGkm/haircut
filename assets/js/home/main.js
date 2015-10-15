78d0e3ce71bd06216db8a364995c21ec507a6136
Ravindra Singh Panwar <rajsinghravi25.87@gmail.com>
2015-10-15 21:09:59 +0530

    DB creadentials

--- assets/js/home/main.js
+++ assets/js/home/main.js
@@ -606,13 +606,12 @@
 
     var retinaLogos = function() {
       var retina = window.devicePixelRatio > 1 ? true : false;
-
-        if(retina) {
-            $('.header .logo').find('img').attr({src:'./images/logo@2x.png',width:'300',height:'150'});
-            $('.home-dark .header .logo').find('img').attr({src:'./images/logo_dark2@2x.png',width:'300',height:'150'});
-            $('.home-2 .header .logo').find('img').attr({src:'./images/logo_3@2x.png',width:'300',height:'150'});
-            $('.header .logo.small').find('img').attr({src:'./images/logo_small@2x.png',width:'454',height:'120'});           
-        }
+        //if(retina) {
+        //    $('.header .logo').find('img').attr({src:base_path+'/logo@2x.png',width:'300',height:'150'});
+        //    $('.home-dark .header .logo').find('img').attr({src:base_path+'/logo_dark2@2x.png',width:'300',height:'150'});
+        //    $('.home-2 .header .logo').find('img').attr({src:base_path+'/logo_3@2x.png',width:'300',height:'150'});
+        //    $('.header .logo.small').find('img').attr({src:base_path+'/logo_small@2x.png',width:'454',height:'120'});
+        //}
     };
     
     var twitterFeed = function () {
